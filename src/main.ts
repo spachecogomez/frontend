94ae896c7eb487194a6ff2a9e413596a10a7632b
Sebastian Pacheco <sebastianpachecogomez@gmail.com>
2017-04-25 23:35:44 -0500

    Adding support for 2 environments

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  console.log('this fucking is prod');
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
